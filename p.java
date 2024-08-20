cf25a3f85477c944b9af5a87cddf4ebd3c159bcb
Koushal Modi <koushalsmodi@gmail.com>
2024-08-19 20:53:35 -0700

    ganpati dada and hanuman dada

--- p.java
+++ p.java
@@ -3,7 +3,8 @@ public class p
     public static void main(String[] args)
     {
         System.out.println("Hello Ganpati Bappa Morya");
-        
+        System.out.println("Jai Hanuman");
+
 
     }
 }
