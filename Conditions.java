4c3d46dccf34c7b4a152629b9dc3900fa6db70f3
Koushal Modi <koushalsmodi@gmail.com>
2024-08-20 20:09:17 -0700

    ganpati and hanuman

---
+++ Conditions.java
@@ -0,0 +1,12 @@
+// if 3 is an odd number, print buzinga
+
+import java.util.*;
+
+public class Conditions{
+    public static void main(String[] args){
+        
+        if (3 % 2 == 0){
+            System.out.println("Buzinga");
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
-public class Main{
-    public static void main(String[] args){
-        int a = 10;
-        int b = 25;
+import java.util.*;
 
-        int multiply = a * b;
-        System.out.println(multiply);
+public class Main{
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        System.out.println(number * 1);
+        System.out.println(number * 2);
+        System.out.println(number * 3);
+        System.out.println(number * 4);
+        System.out.println(number * 5);
+        System.out.println(number * 6);
+        System.out.println(number * 7);
+        System.out.println(number * 8);
+        System.out.println(number * 9);
+        System.out.println(number * 10);
+
+
     }
+
+
+
 }
