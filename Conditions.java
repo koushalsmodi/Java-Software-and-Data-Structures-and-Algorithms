789682e4346a4474db686e07207c62f6ef726129
Koushal Modi <koushalsmodi@gmail.com>
2024-08-20 21:06:11 -0700

    ganpati and hanuman

--- Conditions.java
+++ Conditions.java
@@ -1,12 +1,36 @@
-// if 3 is an odd number, print buzinga
+// if num == 1 -> hello
+// num == 2 -> namaste
+// num == 3 -> bonjour
+
 
 import java.util.*;
 
 public class Conditions{
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        switch(num){
+
+            case 1:{
+                System.out.println("Hello");
+                break;
+
+            }
+
+            case 2: System.out.println("Namaste");
+            break;
+
         
-        if (3 % 2 == 0){
-            System.out.println("Buzinga");
+            
+
+            case 3: {
+                System.out.println("Bonjour");
+                break;
+            }
+            default: System.out.println("Invalid button");
+
         }
     }
 }
