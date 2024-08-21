c9286e6d54f05f29b5ede6e317683dae67790329
Koushal Modi <koushalsmodi@gmail.com>
2024-08-21 13:37:11 -0700

    ganpati and hanuman

--- Loops.java
+++ Loops.java
@@ -1,14 +1,19 @@
-// print a table of a number input by a user
-import java.util.*;
+/*
+ * *****
+ * *****
+ * *****
+ * *****
+ */
+
 public class Loops{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-
-        for (int i = 1; i <= 10; i++){
-            System.out.println(num*i);
+        int n = 4;
+        int m = 5;
+        for (int row = 1; row <= n; row++){
+            for (int col = 1; col <= m; col++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        sc.close();
-
     }
 }
