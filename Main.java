b35a3aebc4019c3084127d00ddfee8e1389ef1df
Koushal Modi <koushalsmodi@gmail.com>
2024-08-20 19:12:35 -0700

    ganpati and hanuman

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.print("Hello World");
+public class Main{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 25;
+
+        int multiply = a * b;
+        System.out.println(multiply);
     }
 }
