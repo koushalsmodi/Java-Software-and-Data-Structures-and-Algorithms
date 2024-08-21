7233f56391d6aa7324ae979124dc232f794aea76
Koushal Modi <koushalsmodi@gmail.com>
2024-08-20 21:11:28 -0700

    ganpati and hanuman

---
+++ Calendar.java
@@ -0,0 +1,57 @@
+import java.util.*;
+// Ask the user to enter the number of the month & print the name of the month. For eg - For ‘1’ print ‘January’, ‘2’ print ‘February’ & so on.
+
+public class Calendar{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter num between 1-12");
+        int num = sc.nextInt();
+        switch(num){
+
+            case 1:
+            System.out.println("January");
+            break;
+            case 2:
+            System.out.println("February");
+            break;
+            case 3:
+            System.out.println("March");
+            break;
+            case 4:
+            System.out.println("April");
+            break;
+            case 5:
+            System.out.println("May");
+            break;
+            case 6:
+            System.out.println("June");
+            break;
+            case 7:
+            System.out.println("July");
+            break;
+            case 8:
+            System.out.println("August");
+            break;
+            case 9:
+            System.out.println("September");
+            break;
+            case 10:
+            System.out.println("October");
+            break;
+            case 11:
+            System.out.println("November");
+            break;
+            case 12:
+            System.out.println("December");
+            break;
+            default:
+            throw new IllegalArgumentException("invalid number");
+
+
+
+
+        }
+        sc.close();
+
+    }
+}
