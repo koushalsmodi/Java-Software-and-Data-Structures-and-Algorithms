1d2f40fd5f82085187b0ad46feda8a0de11c66c4
Koushal Modi <koushalsmodi@gmail.com>
2024-08-23 19:53:35 -0700

    ganpati and hanuman

--- OOPS.java
+++ OOPS.java
@@ -1,37 +1,20 @@
-class Pen{
-    String brand;
-    String colour;
-    String type;
+class Student{
+    String name;
+    int age;
 
-    public void printInfo(){
-        System.out.println("Brand name: "+ this.brand);
-        System.out.println("Colour: " + this.colour);
-        System.out.println("Type: " + this.type);
-        System.out.println();
+    Student(String name, int age){
+        this.name = name;
+        this.age = age;
+        System.out.println(name + " " + age);
     }
+
 
 }
 
 public class OOPS{
     public static void main(String[] args){
-        Pen pen1 = new Pen();
-        pen1.brand = "Pilot";
-        pen1.colour = "Blue";
-        pen1.type = "gel";
-
-        Pen pen2 = new Pen();
-        pen2.brand = "Microsoft";
-        pen2.colour = "yellow";
-        pen2.colour = "ball";
-
-        Pen pen3 = new Pen();
-        pen3.brand = "Amazon";
-        pen3.colour = "red";
-        pen3.type = "balloon";
-
-        pen1.printInfo();
-        pen2.printInfo();
-        pen3.printInfo();
+        Student s1 = new Student("Ganesh", 24);
+        
 
     }
 }
