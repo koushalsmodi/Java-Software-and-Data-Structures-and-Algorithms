88316a40a4dd97d74f7ee19894eb066f9ac4ab0b
Koushal S Modi <124229657+koushalsmodi@users.noreply.github.com>
2024-10-13 21:11:26 -0700

    Add files via upload

---
+++ practice/factorial.java
@@ -0,0 +1,17 @@
+public class factorial {
+    // Write a recursive method to perform binary search on a sorted array.
+
+    public static int sumNatural(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n + sumNatural(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6;
+        System.out.println(sumNatural(a));
+
+    }
+}
