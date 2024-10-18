99b5602094e0294cc0850079cec1fbe07b56b46f
Koushal S Modi <124229657+koushalsmodi@users.noreply.github.com>
2024-10-17 20:42:35 -0700

    Add files via upload

---
+++ practice/Entry.java
@@ -0,0 +1,8 @@
+package MyPriorityQueue;
+
+/** Interface for a key-value pair. */
+public interface Entry<K, V> {
+    K getKey(); // returns the key stored in this entry
+
+    V getValue(); // returns the value stored in this entry
+}
